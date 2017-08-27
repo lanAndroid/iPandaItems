737f4692484071519d776c73bc969329aefd9b34
lanAndroid <369411355@qq.com>
2017-08-27 21:21:11 +0800

    ssss

---
+++ app/src/main/java/com/example/ipandaitems/view/livechina/Ilivechinaview.java
@@ -0,0 +1,13 @@
+package com.example.ipandaitems.view.livechina;
+
+import com.example.ipandaitems.model.entry.livechina.livechinaBean;
+
+/**
+ * Created by 张豫耀 on 2017/8/25.
+ */
+//aaaaaaa
+public interface Ilivechinaview {
+    void succeed(livechinaBean livechinaBean);
+
+    void Failure();
+}
