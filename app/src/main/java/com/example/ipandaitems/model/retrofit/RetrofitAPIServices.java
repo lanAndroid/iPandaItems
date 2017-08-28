1ae4e253a358b78c59a789cf5bb7dffb76d5e480
tian0000 <1173568715@qq.com>
2017-08-28 08:55:56 +0800

    熊猫直播

--- app/src/main/java/com/example/ipandaitems/model/retrofit/RetrofitAPIServices.java
+++ app/src/main/java/com/example/ipandaitems/model/retrofit/RetrofitAPIServices.java
@@ -5,6 +5,9 @@ import com.example.ipandaitems.model.entry.AnnBean;
 import com.example.ipandaitems.model.entry.Bean;
 import com.example.ipandaitems.model.entry.HomeBean;
 import com.example.ipandaitems.model.entry.livechina.livechinaBean;
+import com.example.ipandaitems.model.entry.pandalive.PLAmaPhotoes;
+import com.example.ipandaitems.model.entry.pandalive.PLHome;
+import com.example.ipandaitems.model.entry.pandalive.PLLive;
 import com.example.ipandaitems.utils.UrlUtils;
 
 import java.util.Map;
@@ -39,4 +42,15 @@ public interface RetrofitAPIServices {
     @GET(UrlUtils.LIVECHINAURL)
     Observable<livechinaBean> getLiveChinaGET();
 
+    //    熊猫直播 第一次网络获取
+    @GET(UrlUtils.PANDALIVE)
+    Observable<PLHome> getPandaLiveGET();
+
+    //    熊猫直播 第二次网络获取
+    @GET(UrlUtils.MULITANGLE)
+    Observable<PLLive> getPandaLivesGET();
+
+    //    熊猫直播 精彩一刻
+    @GET(UrlUtils.ORIGINALNEWS)
+    Observable<PLAmaPhotoes> getPLAmaPhotoesGET();
 }
--- app/src/main/java/com/example/ipandaitems/model/retrofit/RetrofitUtils.java
+++ app/src/main/java/com/example/ipandaitems/model/retrofit/RetrofitUtils.java
@@ -5,6 +5,9 @@ import com.example.ipandaitems.model.entry.AnnBean;
 import com.example.ipandaitems.model.entry.Bean;
 import com.example.ipandaitems.model.entry.HomeBean;
 import com.example.ipandaitems.model.entry.livechina.livechinaBean;
+import com.example.ipandaitems.model.entry.pandalive.PLAmaPhotoes;
+import com.example.ipandaitems.model.entry.pandalive.PLHome;
+import com.example.ipandaitems.model.entry.pandalive.PLLive;
 
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
@@ -79,5 +82,22 @@ public class RetrofitUtils {
         annBeanObservable.subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread()).subscribe(observer);
     }
 
+    //    熊猫直播 第一次网络获取
+    public void getPandaLive(Observer<PLHome> observer) {
+        Observable<PLHome> pandaLiveGET = apiServices.getPandaLiveGET();
+        pandaLiveGET.subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread()).subscribe(observer);
+    }
+
+    //    熊猫直播 第二次网络获取
+    public void getPandaLives(Observer<PLLive> observer) {
+        Observable<PLLive> pandaLiveGET = apiServices.getPandaLivesGET();
+        pandaLiveGET.subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread()).subscribe(observer);
+    }
+
+    //    熊猫直播 精彩一刻
+    public void getPLAmaPhotoes(Observer<PLAmaPhotoes> observer) {
+        Observable<PLAmaPhotoes> plAmaPhotoesGET = apiServices.getPLAmaPhotoesGET();
+        plAmaPhotoesGET.subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread()).subscribe(observer);
+    }
 
 }
