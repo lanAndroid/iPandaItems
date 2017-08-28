a190622ead4b21bb372d457593fc30f2ae5675f3
tian0000 <1173568715@qq.com>
2017-08-28 08:47:48 +0800

    熊猫直播

---
+++ app/src/main/java/com/example/ipandaitems/model/entry/pandalive/PLHome.java
@@ -0,0 +1,11 @@
+package com.example.ipandaitems.model.entry.pandalive;
+
+/**
+ * @name yanantian
+ * @motto 莫羡他人谢语花, 腹有诗书气自华
+ * @E-mail 1173568715@qq.com
+ * @WX 15978622391
+ */
+
+public class PLHome {
+}
