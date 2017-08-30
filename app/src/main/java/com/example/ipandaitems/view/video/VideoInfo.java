6c6504b5688bdaf9c3222c18f342724ecba955f1
xiaowuzoodjsa <6031574@qq.com>
2017-08-30 09:15:02 +0800

    videoFragment

--- app/src/main/java/com/example/ipandaitems/view/video/VideoInfo.java
+++ app/src/main/java/com/example/ipandaitems/view/video/VideoInfo.java
@@ -1,8 +1,15 @@
 package com.example.ipandaitems.view.video;
 
+import com.example.ipandaitems.model.entry.TopBean;
+import com.example.ipandaitems.model.entry.TopListBean;
+
 /**
  * Created by 1 on 2017/8/29.
  */
 
 public interface VideoInfo {
+    void onSuccess(TopBean dataBean);
+    void onError(String e);
+    void topListSuccess(TopListBean topListBean);
+    void topListError(String e);
 }
