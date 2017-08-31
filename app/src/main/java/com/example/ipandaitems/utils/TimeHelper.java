dec169bea0fb189a90bcaa58164daecb0582f8c1
xiaogang <512421960@QQ.com>
2017-08-31 08:08:02 +0800

    时间辅助类

---
+++ app/src/main/java/com/example/ipandaitems/utils/TimeHelper.java
@@ -0,0 +1,33 @@
+package com.example.ipandaitems.utils;
+
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+/**
+ * Created by xiaogang on 2017/8/30.
+ */
+
+public class TimeHelper {
+    /**
+     * 返回格式化的当前时间
+     *
+     * @return 返回的时间格式为 2015-03-22
+     */
+    public static String getCurrentData() {
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date date = new Date(getCurrentMillisTime());
+        return format.format(date);
+    }
+
+    /**
+     * 返回当前毫秒数
+     *
+     * @return
+     */
+    public static long getCurrentMillisTime() {
+        Calendar calendar = Calendar.getInstance();
+        long timeInMillis = calendar.getTimeInMillis();
+        return timeInMillis;
+    }
+}
